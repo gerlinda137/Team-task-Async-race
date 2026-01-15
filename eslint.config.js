4e0377b9dcda0cf9094c246b7a3bc526cc24c712
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-01-15 14:18:11 +0400

    feat: add basic project setup

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: ts.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {},
+  },
+];
