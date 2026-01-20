4852ed848541ddb45501cd6e0dac30dc47ff5326
iiamsona <sona22hakobyan@gmail.com>
2026-01-20 14:59:13 +0400

    feat: apis added

--- frontend/src/views/winners/winners-view.ts
+++ frontend/src/views/winners/winners-view.ts
@@ -1,8 +1,18 @@
 import { BaseComponent } from '../../components/ui/base-component';
+import { getWinners } from '../../api/winners';
 
 export class WinnersView extends BaseComponent {
   constructor() {
     super('div', 'winners-view');
-    this.element.innerHTML = `<h1>Winners (0)</h1><h2>Page #2</h2>`;
+    this.render();
+  }
+
+  private async render(): Promise<void> {
+    const { totalCount } = await getWinners(1);
+
+    this.element.innerHTML = `
+      <h1>Winners (${totalCount})</h1>
+      <h2>Page #1</h2>
+    `;
   }
 }
