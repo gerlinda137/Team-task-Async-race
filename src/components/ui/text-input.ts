23160be485d3f2a506780dcedcb3cc11a500883f
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-01-23 19:26:52 +0400

    feat: add getValue()

--- src/components/ui/text-input.ts
+++ src/components/ui/text-input.ts
@@ -1,9 +1,13 @@
 import { BaseComponent } from "./base-component";
 
-export class textInput extends BaseComponent<HTMLInputElement> {
+export class TextInput extends BaseComponent<HTMLInputElement> {
   constructor(placeholder: string, id: string) {
     super("input", "input-text");
     this.element.placeholder = placeholder;
     this.element.id = id;
+  }
+
+  public getValue() {
+    return this.element.value;
   }
 }
