648823c9803e3abf4e2952559df0369bb4e72deb
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-01-23 23:39:45 +0400

    feat: delete car btn

--- src/components/car-item.ts
+++ src/components/car-item.ts
@@ -1,6 +1,7 @@
 import { BaseComponent } from "./ui/base-component";
 import { CarIcon } from "./ui/car-icon";
 import "./car-item.css";
+import { Button } from "./ui/button";
 
 export interface Car {
   name: string;
@@ -9,10 +10,15 @@ export interface Car {
 }
 
 export class CarItem extends BaseComponent<HTMLLIElement> {
-  constructor(car: Car) {
+  private carId: number;
+  private onDeleted: (id: number) => void;
+
+  constructor(car: Car, onDeleted: () => void) {
     super("li", "car-list__item");
+    this.carId = car.id;
     this.element.id = car.id.toString();
     this.render(car);
+    this.onDeleted = onDeleted;
   }
 
   private render(car: Car) {
@@ -20,7 +26,11 @@ export class CarItem extends BaseComponent<HTMLLIElement> {
     carTitle.className = "car__title";
     carTitle.textContent = car.name;
     const carIcon = new CarIcon(car.color);
+    const deleteCarButton = new Button("delete car", "", "button", () =>
+      this.onDeleted(this.carId),
+    );
     this.element.append(carTitle);
+    this.element.append(deleteCarButton.getElement());
     this.element.append(carIcon.getElement());
   }
 }
